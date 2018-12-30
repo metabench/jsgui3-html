7a5175775ab4f53d9a62a8d2b7cff52d6b45e250
James Vickers <james@metabench.com>
2018-12-30 23:19:19 +0000

    0.0.42 - mx dragable can choose which ctrl's event(s) initiates drag     start_action

--- control_mixins/dragable.js
+++ control_mixins/dragable.js
@@ -1,124 +1,12 @@
-/*
-
-var a = arguments;
-		a.l = arguments.length;
-		var sig = get_a_sig(a, 1);
-		var that = this;
-		//console.log('dragable sig', sig);
-		//console.trace();
-		var options = {},
-			mode, drag_start_distance = 4;
-		// options could contain event handlers.
-		//  Not sure about the publish / subscribe model.
-		//   Maybe it would work well.
-		// But allowing event handlers as specified in the options would be good as well.
-		var fn_mousedown, fn_dragstart, fn_dragmove, fn_dragend;
-		var handle_mousedown, handle_dragstart, handle_dragmove, handle_dragend;
-
-		if (sig == '[o]') {
-			options = a[0];
-		}
-
-		// fn_mousedown, fn_begin, fn_move, fn_end
-		if (sig == '[f,f,f,f]') {
-			handle_mousedown = a[0];
-			handle_dragstart = a[1];
-			handle_dragmove = a[2];
-			handle_dragend = a[3];
-		}
-
-
-		if (options.mode) mode = options.mode;
-		//if (options.fn_dragmove) fn_dragmove = options.fn_dragmove;
-		if (options.move) handle_dragmove = options.move;
-		//if (options.fn_dragstart) fn_dragstart = options.fn_dragstart;
-		if (options.start) handle_dragstart = options.start;
-
-		// could have a 'none' mode that does not implement drag behaviour itself, but just shows the events?
-		//  or I think 'events' mode would be a better name because it's saying what it is.
-		//  would be useful for moving objects around according to more specific rules.
-
-		if (mode == 'ghost-copy') {
-			console.log('ghost-copy drag');
-		}
-
-		var body = that.context.body();
-		// raise the events externally.
-		var is_dragging;
-		var pos_mousedown;
-
-		var ghost_clone;
-		var fn_mousemove = function (e_mousemove) {
-			//console.log('e_mousemove', e_mousemove);
-			var pos = [e_mousemove.pageX, e_mousemove.pageY];
-			var pos_offset = [pos[0] - pos_mousedown[0], pos[1] - pos_mousedown[1]];
-
-			//console.log('dist', dist);
-			//console.log('is_dragging ' + is_dragging);
-
-			if (!is_dragging) {
-				var dist = Math.round(Math.sqrt(pos_offset[0] * pos_offset[0] + pos_offset[1] * pos_offset[1]));
-				if (dist >= drag_start_distance) {
-					//console.log('starting drag');
-					is_dragging = true;
-					// in ghost copy mode create the ghost copy
-					if (mode == 'ghost-copy') {
-						ghost_clone = that.absolute_ghost_clone();
-					}
-					if (handle_dragstart) {
-						e_mousemove.control = that;
-						// set the body's css cursor to 'default'
-						//body.style('cursor', 'default');
-						body.add_class('no-text-select')
-						body.add_class('default-cursor');
-						//body.add_class('dragging');
-						handle_dragstart(e_mousemove);
-					}
-				}
-			}
-			if (is_dragging) {
-				// raise the drag event.
-				// could do some of the drag-drop activity depending on the drag mode.
-				//  also want to provide other hooks for functionality.
-				// console.log('fn_dragmove', fn_dragmove);
-				if (handle_dragmove) {
-					e_mousemove.control = that;
-					//console.log('e_mousemove', e_mousemove);
-					handle_dragmove(e_mousemove);
-				}
-			}
-			// Want the offset from the mousedown position.
-		}
-		var fn_mouseup = function (e_mouseup) {
-			//console.log('e_mouseup', e_mouseup);
-			//console.log('pre switch off mousemove, mouseup');
-			// Seems the events are being added too many times.
-			body.off('mousemove', fn_mousemove);
-			body.off('mouseup', fn_mouseup);
-			body.remove_class('no-text-select');
-			body.remove_class('default-cursor');
-			//body.remove_class('dragging');
-		}
-		this.on('mousedown', function (e_mousedown) {
-			//console.log('e_mousedown', e_mousedown);
-			pos_mousedown = [e_mousedown.pageX, e_mousedown.pageY];
-			// position within Control
-			// position within window
-			body.on('mousemove', fn_mousemove);
-			body.on('mouseup', fn_mouseup);
-			body.add_class('no-text-select');
-			is_dragging = false;
-			if (handle_mousedown) {
-				handle_mousedown(e_mousedown);
-			}
-		})
-
-*/
-
 const {
 	prop,
 	field
 } = require('obext');
+
+const {
+	each,
+	tof
+} = require('../lang/essentials');
 
 let dragable = (ctrl, opts = {}) => {
 	//let selection_action = 'mousedown';
@@ -131,11 +19,17 @@ let dragable = (ctrl, opts = {}) => {
 	let {
 		bounds,
 		handle,
-		mode
+		mode,
+		start_action
 	} = opts;
 	// bounds could be a control.
 
 	//control
+
+	start_action = start_action || ['touchstart', 'mousedown'];
+
+	if (tof(start_action) === 'string') start_action = [start_action];
+
 
 	// boundary control
 	let bounds_pos;
@@ -229,8 +123,13 @@ let dragable = (ctrl, opts = {}) => {
 			//console.log('ctrl.position', ctrl.position);
 			//console.log('1) ctrl.pos', ctrl.pos);
 			//console.log('ctrl', ctrl);
-			let new_pos = [item_start_pos[0] - movement_offset[0], item_start_pos[1] - movement_offset[1]];
-			ctrl.pos = new_pos;
+
+
+			//let new_pos = [item_start_pos[0] - movement_offset[0], item_start_pos[1] - movement_offset[1]];
+			//ctrl.pos = new_pos;
+			ctrl.pos = [item_start_pos[0] - movement_offset[0], item_start_pos[1] - movement_offset[1]];
+
+
 			//console.log('2) ctrl.pos', ctrl.pos);
 			//let new_item_pos = 
 		} else {
@@ -248,10 +147,11 @@ let dragable = (ctrl, opts = {}) => {
 				//console.log('bounds_pos', bounds_pos);
 
 				//let new_pos = [item_start_pos[0] + movement_offset[0] + bounds_offset[0] - pos_md_within_ctrl[0] - item_width, item_start_pos[1]];
-				let new_pos = [item_start_pos[0] + movement_offset[0], item_start_pos[1]];
+				//let new_pos = [item_start_pos[0] + movement_offset[0], item_start_pos[1]];
 				//console.log('item_start_pos[1]', item_start_pos[1]);
 
-				ctrl.pos = new_pos;
+				//ctrl.pos = new_pos;
+				ctrl.pos = [item_start_pos[0] + movement_offset[0], item_start_pos[1]];
 			} else {
 				console.log('drag_mode', drag_mode);
 				throw 'NYI';
@@ -295,7 +195,7 @@ let dragable = (ctrl, opts = {}) => {
 			//console.log('bounds_size', bounds_size);
 			//console.log('bounds_pos', bounds_pos);
 			// half_item_width
-			
+
 
 			//let new_pos = [item_start_pos[0] + movement_offset[0] + bounds_pos[0] - pos_md_within_ctrl[0] - half_item_width, item_start_pos[1]];
 			//console.log('item_start_pos', item_start_pos);
@@ -440,30 +340,39 @@ let dragable = (ctrl, opts = {}) => {
 					if (typeof document === 'undefined') {} else {
 						// on activation of control.
 
-						let apply_start_handlers = () => {
+						let apply_start_handlers = (start_action) => {
 							//console.log('apply_start_handlers handle', handle);
 							//console.trace();
 							if (!handle.has_drag_md_handler) {
 								handle.has_drag_md_handler = true;
-								handle.on('touchstart', h_md);
-								handle.on('mousedown', h_md);
+
+								each(start_action, sa => {
+									handle.on(sa, h_md);
+								});
+
+								//handle.on('touchstart', h_md);
+								//handle.on('mousedown', h_md);
 							}
-
-							
 						}
-
 						//console.log('handle.has_drag_md_handler', handle.has_drag_md_handler);
-
 						ctrl.once_active(() => {
 							//console.log('dragable once_active');
-							apply_start_handlers();
+							apply_start_handlers(start_action);
 						});
 
 					}
 				} else {
 					if (typeof document === 'undefined') {} else {
+						(start_action => {
+							each(start_action, sa => {
+								handle.off(sa, h_md);
+							});
+						})(start_action);
+						
+						/*
 						handle.off('touchstart', h_md);
 						handle.off('mousedown', h_md);
+						*/
 						handle.has_drag_md_handler = false;
 					}
 				}
