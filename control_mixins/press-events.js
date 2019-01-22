bb0a415494ce304b07dc249142b2e9fcc60c8cc7
James Vickers <james@metabench.com>
2019-01-22 14:39:35 +0000

    0.0.60 - Further work on press events mixin

--- control_mixins/press-events.js
+++ control_mixins/press-events.js
@@ -6,18 +6,30 @@
     // press-move
     // press-end
 
+    // press-end
+    //  has it moved?
 
     // press-swipe
     // press-drag
+
+    // press-drag-start after the threshold.
+
     // press-move seems enough
+
+    // press-drag after the threshold.
+
+
 
     //  if not moved (by threshold)
     // press-short-hold
 
     // press-grip
 
+    
     // press-hold       (500 ms)
     // press-hold(n)
+
+    // press-hold-end?
 
     // press-long-hold
 
@@ -34,6 +46,17 @@ let press_events = (ctrl) => {
     let movement_offsets;
     let pos_move;
     let movement_offset;
+    let timeout_hold;
+    let drag_started = false;
+
+    let el;
+
+    // The distance moved so far.
+
+    // hold gesture will have a tiny movement threshold (4)
+    const hold_movement_threshold = 4;
+    let move_mag;
+    const ms_short = 500;
 
     const ts = e => {
         if (e.touches.length === 1) {
@@ -42,10 +65,15 @@ let press_events = (ctrl) => {
             e.pos = [e.pageX = e.touches[0].pageX, e.pageY = e.touches[0].pageY];
             //e.preventDefault();
             //console.log('ctrl', ctrl);
+            el = e.el = e.touches[0].target;
             return ps(e);
         }
     }
     const md = e => {
+
+        //let top_el = e.targetTouches[0].target;
+        el = e.el = e.path[0];
+
         return ps(e);
     }
     const mm = e => {
@@ -63,17 +91,36 @@ let press_events = (ctrl) => {
     //    ctrl.raise('pointer-move');
     //}
     const pm = e => {
-        pos_move = [e.pageX, e.pageY];
-
-        e.movement_offset = movement_offset = [pos_move[0] - pos_start[0], pos_move[1] - pos_start[1]];
+        pos_move = e.pos;
+        //console.log('e', e);
+        
+       // pos_move = [e.pageX, e.pageY];
+        //console.log('pos_start', pos_start);
+        e.movement_offset = movement_offset = [e.pos[0] - pos_start[0], e.pos[1] - pos_start[1]];
+        //move_mag = 
+        
         movement_offsets.push(movement_offset);
         e.movement_offsets = movement_offsets;
+        e.move_mag = move_mag = Math.sqrt(Math.pow(movement_offset[0], 2) + Math.pow(movement_offset[1], 2));
+        e.ctrl = ctrl;
         
         ctrl.raise('press-move', e);
+
+        if (move_mag >= 4 && !drag_started) {
+            ctrl.raise('press-drag-start', e);
+            drag_started = true;
+        }
+
+        // press-drag-start
     }
     const pe = e => {
         //ctrl.raise('pointer-move');
         e.movement_offsets = movement_offsets;
+        e.move_mag = move_mag;
+        e.ctrl = ctrl;
+        e.el = el;
+
+        if (timeout_hold) clearTimeout(timeout_hold);
 
         ctrl.raise('press-end', e);
     }
@@ -90,16 +137,18 @@ let press_events = (ctrl) => {
             touchend: te,
             touchmove: tm
         });
+
         e.pos = pos_move;
         return pe(e);
     }
     const ps = e => {
         movement_offsets = [];
+        move_mag = 0;
+        drag_started = false;
         let is_touch_event = !!e.touches;
         e.is_touch_event = is_touch_event;
         //console.log('e', e);
         //console.log('is_touch_event', is_touch_event);
-
         //body.on('')
 
         pos_start = [e.pageX, e.pageY];
@@ -115,7 +164,17 @@ let press_events = (ctrl) => {
                 mousemove: mm
             });
         }
+        e.ctrl = ctrl;
         ctrl.raise('press-start', e);
+
+        // timesout, until lower time threshold
+
+        timeout_hold = setTimeout(() => {
+
+            if (move_mag <= hold_movement_threshold) {
+                ctrl.raise('press-hold', e);
+            }
+        }, ms_short);
     }
     ctrl.on({
         'touchstart': ts,
